b8bdb3dc53f4f20675db011accdd7ea6cc283733
Avnish Patel <avnishpatel.ce@hotmail.com>
2024-11-24 17:56:55 +0530

    1 : find nth Largest number (with/without index) 2 : Remove Duplicate char 3 : Check valid pair

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace CodePractice
     {
         static void Main(string[] args)
         {
+            #region String Utility function
             //StringUtil.DisplayStart();
             //StringUtil.Get(1);
             //StringUtil.Get(1.0);
@@ -17,8 +18,8 @@ namespace CodePractice
             //StringUtil.NullCoalesing_c();
             //StringUtil.NullCoalesing_bc();
             //StringUtil.NullCoalesing_abc();
-
-            //StringUtil.SwitchCase();
+            //StringUtil.SwitchCase(); 
+            #endregion
 
             //ListUtil.CheckElementInList();
 
@@ -56,10 +57,80 @@ namespace CodePractice
             #endregion
 
 
-            ColleaguePrg.GetFibonacciSerice();
+            //ColleaguePrg.GetFibonacciSerice();
+            //ColleaguePrg.DisplayFibonacciNumberByRecursion();
 
-            ColleaguePrg.DisplayFibonacciNumberByRecursion();
-            
+
+            //TempClass.FactorialNumber(5);
+
+            #region Is Palindrom String
+            //TempClass.IsPalindromString("AbA");
+            //TempClass.IsPalindromString("AbcbA");
+            //TempClass.IsPalindromString("AbccbA");
+            //TempClass.IsPalindromString("AbcbA");
+            //TempClass.IsPalindromString("AbcbA1"); 
+            #endregion
+
+            #region GetFibonacci Number : All all 
+            //TempClass.GetFibonacciString(1);
+            //TempClass.GetFibonacciString(2);
+            //TempClass.GetFibonacciString(3);
+            //TempClass.GetFibonacciString(4);
+            //TempClass.GetFibonacciString(5);
+            //TempClass.GetFibonacciString(6);
+            //TempClass.GetFibonacciString(7);
+            //TempClass.GetFibonacciString(8); 
+            #endregion
+
+
+            #region Get Unique Char From String
+            //TempClass.Get1stUniqueCharFromString("avanish Patel");
+            //TempClass.GetNonRepeatedCharFromString("avanish Patel");
+            #endregion
+
+
+            #region Reversal OfString
+            //TempClass.ReverseOfString("Avnish");
+            //Console.WriteLine();
+
+            //TempClass.ReverseOfString("Avnish Patel");
+            //Console.WriteLine();
+
+            //TempClass.ReverseOfString("AbcbA");
+            //TempClass.ReverseOfWordFromStatement("Avnish Patel Rmp"); 
+            #endregion
+
+            #region Reverse Number
+            //Console.WriteLine(TempClass.ReverseNumber(123));
+            //Console.WriteLine(TempClass.ReverseNumber(1034));
+            #endregion
+
+
+            #region find nth Largest number (with/without index)
+            //TempClass.LargestNumberWithIndexFromArray([40,70, 20, 10, 60, 30, 50]);
+            //TempClass.LargestNumberWithIndexFromArray([40, 20,70, 10, 60, 30, 50]);
+
+
+            //TempClass.nthLargestNumberFromArray(6);
+            //TempClass.nthLargestNumberFromArray(8); 
+            #endregion
+
+            #region Duplicate char
+            //TempClass.RemoveDuplicateValueFromArray(); 
+            //TempClass.ListDuplicateValueFromArray();
+
+            //TempClass.RemoveDuplicateValueButContainAtLeatOnceFromArray();
+            //TempClass.RemoveDuplicateValueButNotUsenewArray(); 
+            #endregion
+
+
+            #region Is BracketPair : {[()]} / {[)(]} / {[[()]}
+            //TempClass.BracketPair("{[()]}");
+            //TempClass.BracketPair("{[)(]}");
+            //TempClass.BracketPair("{[([)]}");
+            //TempClass.BracketPair("{[[()]}"); 
+            #endregion
+
         }
     }
 
