a38f6143fedad32ae4dbe0e0c9a6364e0f521b8e
Avnish Patel <avnishpatel.ce@hotmail.com>
2025-05-10 20:07:25 +0530

    concept of Inheritance and overriding

---
+++ Opps/Polymorphism.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CodePractice.Opps
+{
+    public class ParentCls
+    {
+        public void Test1()
+        {
+            Console.WriteLine("From Parent class: Test 1()");
+        }
+        public virtual void Test2()
+        {
+            Console.WriteLine("From Parent class: Test 2()");
+        }
+    }
+
+
+    public class ChildCls : ParentCls
+    {
+
+        #region From Childclass, Call ParentMathod
+        public void FromChildCallParentMathod()
+        {
+            base.Test1();
+            base.Test2();
+        }
+        #endregion
+        public new void Test1()
+        {
+            Console.WriteLine("From Child class: Test 1");
+        }
+
+        public override void Test2()
+        {
+            Console.WriteLine("From Child class: Test 2");
+        }
+
+        public void Test3()
+        {
+            Console.WriteLine("From Child class: Test 3");
+        }
+
+    }
+
+
+}
